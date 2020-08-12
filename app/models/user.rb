556272b8855e723c209a42e8b6be160588140cde
takeru89 <t.sada1989@gmail.com>
2020-08-12 19:52:14 +0900

    validation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,}\z/i.freeze
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: { case_sensitive: false},
+                      format: { with: EMAIL_REGEX}
+    validates :password, length: { minimum: 8}, format: { with: PASSWORD_REGEX },
+                         confirmation: true
+  end
 end
