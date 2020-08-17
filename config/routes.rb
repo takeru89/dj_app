694972104ec1a42d68d5881a69c678b79efb3e2e
takeru89 <t.sada1989@gmail.com>
2020-08-17 13:26:24 +0900

    routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'words#index'
-  resources :words
+  resources :words do
+    collection do
+      get 'search'
+    end
+  end
 end
