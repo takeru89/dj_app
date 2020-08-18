e704122f7b6af5068bddfe37f8cbd53ff071edc8
takeru89 <t.sada1989@gmail.com>
2020-08-18 15:14:39 +0900

    MyPage一覧からの編集・削除機能

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,7 @@
 class WordsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :search]
-  before_action :set_word, only: [:show, :edit, :update, :destroy]
+  before_action :set_word, only: [:show, :edit, :update, :destroy,
+                                  :from_mypage_edit, :from_mypage_update, :from_mypage_destroy]
 
   def index
     @words = Word.includes(:user).order('created_at DESC').limit(200)
@@ -46,6 +47,26 @@ class WordsController < ApplicationController
     method = params[:search_method]
     @word = params[:search_word]
     @words = Word.search(method, @word)
+  end
+
+  def from_mypage_edit
+  end
+
+  def from_mypage_update
+    if @word.update(word_params)
+      redirect_to user_path(@word.user_id)
+    else
+      render :edit
+    end
+  end
+
+  def from_mypage_destroy
+    if @word.destroy
+      redirect_to user_path(@word.user_id)
+    else
+      flash.now[:alert] = 'Delete Failed'
+      render @word
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+    member do
+      get 'from_mypage_edit'
+      patch 'from_mypage_update'
+      put 'from_mypage_update'
+      delete 'from_mypage_destroy'
+    end
   end
   resources :users, only: :show
 end
