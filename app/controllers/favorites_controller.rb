2674128a72d925fe6aa284bdf126c8c5341b0721
takeru89 <t.sada1989@gmail.com>
2020-08-19 13:33:59 +0900

    favoritesコントローラー編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,16 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.where(user_id: current_user.id)
+  end
+
+  def create
+    @word = Word.find(params[:word_id])
+    @favorite = Favorite.new(user_id: current_user.id, word_id: @word.id)
+    @favorite.save
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+  end
 end
