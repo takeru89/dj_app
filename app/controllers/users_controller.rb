1bf7847c8349cb8b0ccf86abf9a1a1a43541188e
takeru89 <t.sada1989@gmail.com>
2020-08-22 14:39:02 +0900

    usersコントローラーのキャッシュ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_cache_buster
 
   def show
     @nickname = current_user.nickname
     @words = current_user.words.page(params[:page]).per(10)
+  end
+
+  private
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
