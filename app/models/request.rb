b57afc98179650fb496e4f5b57c521f68c59757b
takeru89 <t.sada1989@gmail.com>
2020-08-29 10:31:35 +0900

    requestsテーブルからのデータ取得設定

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,10 @@ class Request < ApplicationRecord
   belongs_to :user
 
   validates :request, presence: true
+
+  def self.wrequest
+    to = Time.current
+    from = (to - 6.day).at_beginning_of_day
+    @requests = Request.where(created_at: from...to)
+  end
 end
