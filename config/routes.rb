a740bbd3f4232d91a7f3a21295b5b737380256d3
takeru89 <t.sada1989@gmail.com>
2020-08-29 09:51:25 +0900

    routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'words#index'
+
   resources :words do
     collection do
       get 'search'
@@ -13,7 +14,10 @@ Rails.application.routes.draw do
     end
     resources :favorites, only: [:create, :destroy]
   end
+
   resources :users, only: :show do
     resources :favorites, only: [:index]
   end
+
+  resources :requests, only: [:create, :destroy]
 end
